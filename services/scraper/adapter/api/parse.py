2aeae54272e605444c22af29a346349b05dc56a5
Ivan <iv.cherepanov@yandex.ru>
2023-11-16 14:01:42 +0300

    scraper: добавление end-point'ов по скрапингу

---
+++ services/scraper/adapter/api/parse.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException, APIRouter
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+
+from domain.models.url import OuterData
+from domain.services.scraper import parse_all_urls
+
+router = APIRouter()
+
+
+@router.get("/health")
+def read_root():
+    return {"Hello": "World"}
+
+
+@router.post("/process_urls/")
+async def process(list_urls: OuterData):
+    # todo: middleware to check url; logg data
+    data = await parse_all_urls(urls=list_urls.urls)
+    for url in list_urls:
+        print(url)
+    return JSONResponse(content=jsonable_encoder(data))
---
+++ services/scraper/main.py
@@ -0,0 +1,33 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from adapter.api.parse import router as user_router
+from inject import settings, get_cors_address
+
+app = FastAPI()
+app.include_router(user_router)
+
+origins = get_cors_address()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.on_event("startup")
+# async def startup():
+#     await postgres_db.connect()
+#
+#
+# @app.on_event("shutdown")
+# async def shutdown():
+#     # Закрытие соединений с базами данных
+#     await postgres_db.close()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=settings.app_port)
